03057a162420562b6c1bff070b25a67aa86de288
Evan Kirkiles <evkirkiles@gmail.com>
2018-06-30 00:50:56 -0400

    Began work on restructuring and documenting code.

---
+++ Data-Handling/YahooFinanceCSVReader.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Evan Kirkiles on 6/29/18.
+//
+
+// Includes
+#include "YahooFinanceCSVReader.hpp"
+
+//C
---
+++ cmake-build-debug/CMakeFiles/algobacktesterv2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algobacktesterv2.dir/main.cpp.o"
+  "CMakeFiles/algobacktesterv2.dir/Data-Handling/YahooFinanceCSVReader.cpp.o"
+  "algobacktesterv2.pdb"
+  "algobacktesterv2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algobacktesterv2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
